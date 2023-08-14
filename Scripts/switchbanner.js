a68104371b3d84983e80ffef4364c2f0e58064ce
Hlebes <glebdmitrienko@gmail.com>
2023-08-15 00:32:43 +0300

    lower series update

--- Scripts/switchbanner.js
+++ Scripts/switchbanner.js
@@ -1,19 +1,25 @@
+let ignore = false;
 function SwitchBanner(el) {
-    document.getElementById("viewcover").style.opacity = 1;
-    let thumb = el.children[0].innerHTML;
-    let logo = el.children[1].innerHTML;
-    let desc = el.children[2].innerHTML;
-    let linkto = el.children[3].innerHTML;
-    console.log(document.getElementById("series2"));
-    setTimeout(function() {
-        document.getElementById("series2").getElementsByClassName("bgcontent")[0].style = "background-image: url('" + thumb + "');";
-        document.getElementsByClassName("series2desc")[0].getElementsByTagName("a")[0].href = linkto;
-        document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2logo")[0].src = logo;
-        console.log("url('" + thumb + "');");
-        document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2desctext")[0].innerHTML = desc;
-        document.getElementsByClassName("series2desc")[0].getElementsByClassName("link")[0].href = linkto;
+    if (!ignore)
+    {
+        ignore = true;
+        document.getElementById("viewcover").style.opacity = 1;
+        let thumb = el.children[0].innerHTML;
+        let logo = el.children[1].innerHTML;
+        let desc = el.children[2].innerHTML;
+        let linkto = el.children[3].innerHTML;
+        console.log(document.getElementById("series2"));
         setTimeout(function() {
-            document.getElementById("viewcover").style.opacity = 0;
-        }, 150)
-    }, 300)
+            document.getElementById("series2").getElementsByClassName("bgcontent")[0].style = "background-image: url('" + thumb + "');";
+            document.getElementsByClassName("series2desc")[0].getElementsByTagName("a")[0].href = linkto;
+            document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2logo")[0].src = logo;
+            console.log("url('" + thumb + "');");
+            document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2desctext")[0].innerHTML = desc;
+            document.getElementsByClassName("series2desc")[0].getElementsByClassName("link")[0].href = linkto;
+            setTimeout(function() {
+                document.getElementById("viewcover").style.opacity = 0;
+            }, 150)
+        }, 300)
+        ignore = false;
+    }
 }
