68db8152b2e4c661bcb919ec298e7b0e63017ad5
Hlebes <glebdmitrienko@gmail.com>
2023-08-15 00:14:36 +0300

    lower series update

--- Scripts/switchbanner.js
+++ Scripts/switchbanner.js
@@ -12,9 +12,9 @@ function SwitchBanner(el) {
         console.log("url('" + thumb + "');");
         document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2desctext")[0].innerHTML = desc;
         document.getElementsByClassName("series2desc")[0].getElementsByClassName("link")[0].href = linkto;
-    }, 40)
+    }, 200)
     document.getElementById("viewcover").style.opacity = 1;
     setTimeout(function() {
         document.getElementById("viewcover").style.opacity = 0;
-    }, 10)
+    }, 150)
 }
