d32371598a679d1e06c58d69a6c7d70cf41474ef
Hlebes <glebdmitrienko@gmail.com>
2023-08-14 23:54:21 +0300

    lower series update

--- Scripts/switchbanner.js
+++ Scripts/switchbanner.js
@@ -4,16 +4,16 @@ function SwitchBanner(el) {
     let logo = el.children[1].innerHTML;
     let desc = el.children[2].innerHTML;
     let linkto = el.children[3].innerHTML;
-    console.log(document.getElementById(series2));
+    console.log(document.getElementById("series2"));
     setTimeout(function() {
-        document.getElementById(series2).getElementsByClassName(bgcontent)[0].style.backgroundImage = thumb;
-        document.getElementsByClassName(series2desc)[0].getElementsByTagName(a)[0].href = linkto;
-        document.getElementsByClassName(series2desc)[0].getElementsByClassName(series2logo)[0].src = logo;
-        document.getElementsByClassName(series2desc)[0].getElementsByClassName(series2desctext)[0].innerHTML = desc;
-        document.getElementsByClassName(series2desc)[0].getElementsByClassName(link)[0].href = linkto;
+        document.getElementById("series2").getElementsByClassName(bgcontent)[0].style.backgroundImage = thumb;
+        document.getElementsByClassName("series2desc")[0].getElementsByTagName(a)[0].href = linkto;
+        document.getElementsByClassName("series2desc")[0].getElementsByClassName(series2logo)[0].src = logo;
+        document.getElementsByClassName("series2desc")[0].getElementsByClassName(series2desctext)[0].innerHTML = desc;
+        document.getElementsByClassName("series2desc")[0].getElementsByClassName(link)[0].href = linkto;
     }, 40)
-    document.getElementById(viewcover).style.opacity = 1;
+    document.getElementById("viewcover").style.opacity = 1;
     setTimeout(function() {
-        document.getElementById(viewcover).style.opacity = 0;
+        document.getElementById("viewcover").style.opacity = 0;
     }, 10)
 }
