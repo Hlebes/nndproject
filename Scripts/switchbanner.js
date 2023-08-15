a199120684ca38918eb4467029ff1480f62968a4
Hlebes <glebdmitrienko@gmail.com>
2023-08-15 11:28:13 +0300

    mobiletest

--- Scripts/switchbanner.js
+++ Scripts/switchbanner.js
@@ -1,26 +1,30 @@
 let ignore = false;
+let element;
 function SwitchBanner(el) {
     if (!ignore)
     {
-        ignore = true;
-        document.getElementById("viewcover").style.opacity = 1;
-        let thumb = el.children[0].innerHTML;
-        let logo = el.children[1].innerHTML;
-        let desc = el.children[2].innerHTML;
-        let linkto = el.children[3].innerHTML;
-        console.log(document.getElementById("series2"));
-        setTimeout(function() {
-            document.getElementById("series2").getElementsByClassName("bgcontent")[0].style = "background-image: url('" + thumb + "');";
-            document.getElementsByClassName("series2desc")[0].getElementsByTagName("a")[0].href = linkto;
-            document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2logo")[0].src = logo;
-            console.log("url('" + thumb + "');");
-            document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2desctext")[0].innerHTML = desc;
-            document.getElementsByClassName("series2desc")[0].getElementsByClassName("link")[0].href = linkto;
-            // setTimeout(function() {
-                //document.getElementById("viewcover").style.opacity = 0;
-                //ignore = false;
-            // }, 150)
-        }, 300)
+        if (element != el) {
+            ignore = true;
+            document.getElementById("viewcover").style.opacity = 1;
+            let thumb = el.children[0].innerHTML;
+            let logo = el.children[1].innerHTML;
+            let desc = el.children[2].innerHTML;
+            let linkto = el.children[3].innerHTML;
+            console.log(document.getElementById("series2"));
+            setTimeout(function() {
+                document.getElementById("series2").getElementsByClassName("bgcontent")[0].style = "background-image: url('" + thumb + "');";
+                document.getElementsByClassName("series2desc")[0].getElementsByTagName("a")[0].href = linkto;
+                document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2logo")[0].src = logo;
+                console.log("url('" + thumb + "');");
+                document.getElementsByClassName("series2desc")[0].getElementsByClassName("series2desctext")[0].innerHTML = desc;
+                document.getElementsByClassName("series2desc")[0].getElementsByClassName("link")[0].href = linkto;
+                // setTimeout(function() {
+                    //document.getElementById("viewcover").style.opacity = 0;
+                    //ignore = false;
+                // }, 150)
+            }, 300)
+            element = el;
+        }
     }
     else {
         console.log("Ignored function")
@@ -28,11 +32,6 @@ function SwitchBanner(el) {
 }
 
 function RemoveCover() {
-    document.getElementById("viewcover").style.opacity = 0;
-    ignore = false;
-}
-
-function ErrCover() {
     document.getElementById("viewcover").style.opacity = 0;
     ignore = false;
 }
